5cadb21f266d33d49581ae880c8cdc7820a3e099
ramonpulcherio <ramonpulcherio@gmail.com>
2022-03-04 12:50:18 -0300

    Terceiro Commit

--- src/com/ramon/desafiocapgemini/Questão3.java
+++ src/com/ramon/desafiocapgemini/Questão3.java
@@ -1,10 +1,20 @@
 package com.ramon.desafiocapgemini;
 
+import java.util.Scanner;
+
 public class Questão3 {
 
 	public static void main(String[] args) {
 	
-		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digita a frase escolhida? ");
+		String frase = sc.nextLine();
+
+		System.out.println(frase.replace(" ", ""));
+		String[] grid = new String[frase.length()];
+
+		double raiz = Math.ceil(Math.sqrt(frase.length()));
 		
 		
 	}
